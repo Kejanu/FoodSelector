9c46104fd632cc2993bd95f9502846474f4f8aaa
Kejanu <kurznerk@gmail.com>
2022-05-18 17:35:27 +0200

    first commit

---
+++ src/main/java/de/kejanu/foodselector/HelloApplication.java
@@ -0,0 +1,9 @@
+package de.kejanu.foodselector;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
